4d0537c4db9a2ec40c0da9a3b28233ef6256b252
Nika Pairazian <nika.pairazian@gmail.com>
2021-05-14 18:58:50 +0400

    Slider

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,22 @@ let navLinks = document.querySelector('#nav-links');
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('show')
 })
+
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+
+document.querySelector('.slider-next').addEventListener('click', function(){
+    offset +=  256;
+    if (offset > 768) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector('.slider-prev').addEventListener('click', function () {
+    offset -= 256;
+    if (offset < 0) {
+        offset = 768;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
